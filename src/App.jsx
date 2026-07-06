19a8e39642a732a1236aead2ac70c551d82169d2
Karthik Swamy <getspk@gmail.com>
2026-07-06 18:40:48 -0500

    Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-slate-100">
-      <h1 className="text-4xl font-bold text-blue-600 underline">
+    <div className="flex items-center justify-center h-screen border-2 bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
         Tailwind works!
       </h1>
     </div>
